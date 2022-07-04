068127a6dded42232c33481d1005e4e779b5768b
AlexanderBeahm <alexanderbeahm@gmail.com>
2022-07-04 14:50:44 -0500

    Add basic validation interface for leverage in services.

---
+++ RadancyBanking.Services/Validation/ITransactionValidation.cs
@@ -0,0 +1,14 @@
+﻿using RadancyBanking.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadancyBanking.Services.Validation
+{
+    internal interface ITransactionValidation
+    {
+        bool Validate(UserAccount account, AccountTransaction transaction);
+    }
+}
