2ab62f485adf046b7d0dd0250a62a0d1b5282ad4
AlexanderBeahm <alexanderbeahm@gmail.com>
2022-07-04 10:29:38 -0500

    Removing template WeathController in place of empty UserController.
    Adding basic project structure for API.

---
+++ RadancyBanking/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RadancyBanking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public ActionResult Ping()
+        {
+            _logger.LogDebug("Pinged UserController");
+            return Ok();
+        }
+    }
+}
