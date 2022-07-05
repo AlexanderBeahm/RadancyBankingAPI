9756b39aa7e4230797c3c278f9473a3ac25c8b5e
AlexanderBeahm <alexanderbeahm@gmail.com>
2022-07-05 14:14:03 -0500

    Add validator tests.

--- RadancyBanking.Services.Tests/ValidatorFactoryTests.cs
+++ RadancyBanking.Services.Tests/ValidatorFactoryTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RadancyBanking.Services.Validation;
+using RadancyBanking.Services.Validation.Implementations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,20 @@ namespace RadancyBanking.Services.Tests
 {
     public class ValidatorFactoryTests
     {
+        [Fact]
+        public void GenerateWithdrawalFactorySuccess() 
+        {
+            IValidatorFactory validatorFactory = new ValidatorFactory();
+            var validator = validatorFactory.GenerateTransactionValidator(Enumerations.TransactionType.Withdrawal);
+            Assert.IsType<WithdrawalTransactionValidation>(validator);
+        }
+
+        [Fact]
+        public void GenerateDepositFactorySuccess()
+        {
+            IValidatorFactory validatorFactory = new ValidatorFactory();
+            var validator = validatorFactory.GenerateTransactionValidator(Enumerations.TransactionType.Deposit);
+            Assert.IsType<WithdrawalTransactionValidation>(validator);
+        }
     }
 }
---
+++ RadancyBanking.Services.Tests/ValidatorTests.cs
@@ -0,0 +1,82 @@
+﻿using RadancyBanking.DomainModels;
+using RadancyBanking.Services.Validation.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadancyBanking.Services.Tests
+{
+    public class ValidatorTests
+    {
+        private WithdrawalTransactionValidation withdrawalValidator = new WithdrawalTransactionValidation();
+        private DepositTransactionValidation depositValidator = new DepositTransactionValidation();
+
+        [Fact]
+        public void WithdrawalValidationSuccess()
+        {
+            var account = new UserAccount { Balance = 10000m };
+            var transaction = new WithdrawalTransaction { Amount = 100m };
+
+            var result = withdrawalValidator.Validate(account, transaction);
+            Assert.True(result.Item1);
+            Assert.True(string.IsNullOrWhiteSpace(result.Item2));
+        }
+
+        [Fact]
+        public void DepositValidationSuccess()
+        {
+            var account = new UserAccount { Balance = 10000m };
+            var transaction = new WithdrawalTransaction { Amount = 100m };
+
+            var result = depositValidator.Validate(account, transaction);
+            Assert.True(result.Item1);
+            Assert.True(string.IsNullOrWhiteSpace(result.Item2));
+        }
+
+        [Fact]
+        public void OverdraftFullAccountFailure()
+        {
+            var account = new UserAccount { Balance = 10000m };
+            var transaction = new WithdrawalTransaction { Amount = 1000000m };
+
+            var result = withdrawalValidator.Validate(account, transaction);
+            Assert.False(result.Item1);
+            Assert.False(string.IsNullOrWhiteSpace(result.Item2));
+        }
+
+        [Fact]
+        public void OverdraftAccountMaximumPercentageFailure()
+        {
+            var account = new UserAccount { Balance = 10000m };
+            var transaction = new WithdrawalTransaction { Amount = 9001m };
+
+            var result = withdrawalValidator.Validate(account, transaction);
+            Assert.False(result.Item1);
+            Assert.False(string.IsNullOrWhiteSpace(result.Item2));
+        }
+
+        [Fact]
+        public void OverdraftAccountMinimumFailure()
+        {
+            var account = new UserAccount { Balance = 10000m };
+            var transaction = new WithdrawalTransaction { Amount = 9901m };
+
+            var result = withdrawalValidator.Validate(account, transaction);
+            Assert.False(result.Item1);
+            Assert.False(string.IsNullOrWhiteSpace(result.Item2));
+        }
+
+        [Fact]
+        public void DepositMaximumFailure()
+        {
+            var account = new UserAccount { Balance = 10000m };
+            var transaction = new DepositTransaction { Amount = 1000000m };
+
+            var result = depositValidator.Validate(account, transaction);
+            Assert.False(result.Item1);
+            Assert.False(string.IsNullOrWhiteSpace(result.Item2));
+        }
+    }
+}
