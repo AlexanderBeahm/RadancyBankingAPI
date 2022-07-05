b44ea9de0c5c8077323363b860bb6441f28356d9
AlexanderBeahm <alexanderbeahm@gmail.com>
2022-07-05 14:16:09 -0500

    Adding comments to services tests.

--- RadancyBanking.Services.Tests/AccountServiceTests.cs
+++ RadancyBanking.Services.Tests/AccountServiceTests.cs
@@ -8,5 +8,6 @@ namespace RadancyBanking.Services.Tests
 {
     public class AccountServiceTests
     {
+        //If had more time, would flesh out account service tests with an implemented Moq of the repository to return specific data for service to handle.
     }
 }
--- RadancyBanking.Services.Tests/UserServiceTests.cs
+++ RadancyBanking.Services.Tests/UserServiceTests.cs
@@ -8,5 +8,6 @@ namespace RadancyBanking.Services.Tests
 {
     public class UserServiceTests
     {
+        //If had more time, would flesh out account service tests with an implemented Moq of the repository to return specific data for service to handle.
     }
 }
